3ff4438e1d1bc05c6a95402ca2eb7ca54a342e05
faroukm2002 <faroukm238@gamil.com>
2024-03-02 03:32:02 +0200

    create admin controller create addAdmin

---
+++ src/modules/admin/admin.controller.js
@@ -0,0 +1,23 @@
+import { adminModel } from "../../../database/models/admin.models.js"
+
+
+
+// Add Admin
+const addAdmin = catchError(async(req,res,next) => {
+    const Admin =new adminModel(req.body)
+    await Admin.save()
+    res.status(201).json({message:"Done",Admin})
+
+}) 
+
+
+
+
+
+
+export {
+ addAdmin,
+
+}
+
+  
