fd68b880cf3821c5ac6fe662fd05cfcd4df3b50c
faroukm2002 <faroukm238@gamil.com>
2024-04-29 03:04:45 +0300

    create teacherschema

---
+++ database/models/Academic/question.models.js
@@ -0,0 +1,45 @@
+import { Schema,Types,model } from "mongoose"
+
+const questionSchema = new Schema({
+    
+    question:{
+            type:String,
+            required:true,
+            trim:true,
+    },
+
+optionA: {
+    type:String,
+    required: true
+},
+
+optionB: {
+    type:String,
+    required: true
+},
+optionC: {
+    type:String,
+    required: true
+},
+optionD: {
+    type:String,
+    required: true
+},
+
+correctAnswer: {
+    type:String,
+    required: true
+},
+Incorrect: {
+    type:String,
+    default: false
+},
+createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'teacher',
+    required: true
+},
+ 
+},{timeStamp:true});
+
+export const questionModel=model('question',questionSchema)
