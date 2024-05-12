c049acc5ba8c8387e200937455abc3de8e0a7339
faroukm2002 <faroukm238@gamil.com>
2024-05-12 05:15:12 +0300

    create yeargroupSchema

---
+++ database/models/Academic/yeargroup.models.js
@@ -0,0 +1,27 @@
+import { Schema,Types,model } from "mongoose"
+
+const yeargroupSchema = new Schema({
+    name:{
+        type: String,
+        required: [true, 'Name of year is required'],
+        trim: true,
+    },
+    academicYaer:{
+        type:Schema.Types.ObjectId,
+        ref:"academicYear",
+        required:true
+        },
+    duration:{
+        type: String,
+        required: true,
+        default: "3 months"
+    },
+    createdby: {
+        type: Schema.Types.ObjectId,
+        ref: 'admin',
+        required: true
+    },
+  
+},{ timeStamp: true });
+
+export const yeargroupModel = model('yeargroup', yeargroupSchema);
